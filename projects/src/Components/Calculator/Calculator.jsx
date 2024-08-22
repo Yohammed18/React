b93d7fab42e703e8ce1a90b6480095aaffdcfc45
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-08-22 15:22:28 -0400

    Updated css styling

--- projects/src/Components/Calculator/Calculator.jsx
+++ projects/src/Components/Calculator/Calculator.jsx
@@ -81,58 +81,60 @@ function Calculator() {
   return (
     <div className='container'>
       <h1 className="text-center">Calculator App</h1>
-      <div className="container text-center mt-5 ">
-        <div className="row row-cols-8" >
-            <div className="col-1"></div>
-            <div className="col-7">
-                <span style={{display: 'inline-flex'}}>
-                    <input type="text" className='form-control me-3' placeholder={input} value={input} onChange={(e) => setInput(e.target.value)}/>
-                    <button className="btn btn-danger " onClick={clear} style={{fontSize: "20px"}}>Clear</button>
-                </span>
+      <div className="container">
+        <div className="mt-5 ">
+            <div className="row row-cols-6" >
+                <div className="col-sm-2"></div>
+                <div className="col-5">
+                    <span style={{display: 'inline-flex'}}>
+                        <input type="text" className='form-control me-3' placeholder={input} value={input} onChange={(e) => setInput(e.target.value)}/>
+                        <button className="btn btn-danger " onClick={clear} style={{fontSize: "20px"}}>Clear</button>
+                    </span>
+                </div>
+                <div className="col-sm-2"></div>
             </div>
+            <div className="row row-cols-6" >
+                <div className="col"></div>
+                {firstRow ? firstRow.map((first, index) => {
+                    return <div className="col cal-box" key={index} onClick={() => clickButton(first)}
+                    >{first}</div>
+                }) : (
+                    <p>Loading....</p>
+                )}
+                <div className="col"></div>
+            </div>
+            <div className="row row-cols-6" >
+                <div className="col"></div>
+                {secondRow ? secondRow.map((second, index) => {
+                    return <div className="col cal-box" key={index} onClick={() => clickButton(second)}>{second}</div>
+                }) : (
+                    <p>Loading....</p>
+                )}
+                <div className="col"></div>
+            </div>
+            <div className="row row-cols-6" >
+                <div className="col"></div>
+                {thirdRow ? thirdRow.map((third, index) => {
+                    return <div className="col cal-box" key={index} onClick={() => clickButton(third)}>{third}</div>
+                }) : (
+                    <p>Loading....</p>
+                )}
+                <div className="col"></div>
+            </div>
+            <div className="row row-cols-6" >
+                <div className="col"></div>
+                {fourthRow ? fourthRow.map((fourth, index) => {
+                    return (
+                    <div className="col cal-box" key={index} onClick={() => clickButton(fourth)}>{fourth}</div>
+                )
+                }) : (
+                    <p>Loading....</p>
+                )}
+                <div className="col"></div>
+            </div>
+        
         </div>
-        <div className="row row-cols-6" >
-            <div className="col"></div>
-            {firstRow ? firstRow.map((first, index) => {
-                return <div className="col cal-box" key={index} onClick={() => clickButton(first)}
-                >{first}</div>
-            }) : (
-                <p>Loading....</p>
-            )}
-            <div className="col"></div>
-        </div>
-        <div className="row row-cols-6" >
-            <div className="col"></div>
-            {secondRow ? secondRow.map((second, index) => {
-                return <div className="col cal-box" key={index} onClick={() => clickButton(second)}>{second}</div>
-            }) : (
-                <p>Loading....</p>
-            )}
-            <div className="col"></div>
-        </div>
-        <div className="row row-cols-6" >
-            <div className="col"></div>
-            {thirdRow ? thirdRow.map((third, index) => {
-                return <div className="col cal-box" key={index} onClick={() => clickButton(third)}>{third}</div>
-            }) : (
-                <p>Loading....</p>
-            )}
-            <div className="col"></div>
-        </div>
-        <div className="row row-cols-6" >
-            <div className="col"></div>
-            {fourthRow ? fourthRow.map((fourth, index) => {
-                return (
-                <div className="col cal-box" key={index} onClick={() => clickButton(fourth)}>{fourth}</div>
-            )
-            }) : (
-                <p>Loading....</p>
-            )}
-            <div className="col"></div>
-        </div>
-      
       </div>
-
       <hr className='mt-5 mb-5' />
     </div>
   )
